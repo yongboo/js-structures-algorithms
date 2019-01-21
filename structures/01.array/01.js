f282936e4219564bfe165929cfa9f8ebf5139780
yongboo <shanyongbo2008@gmail.com>
2019-01-21 21:55:44 +0800

    update Array demo

--- structures/01.array/01.js
+++ structures/01.array/01.js
@@ -1,4 +1,4 @@
-// 1. 数组的创建方式
+// 1 数组的创建方式
 
 // 1.1 []操作符 
 let a1 = [1, 2, 3]
@@ -22,11 +22,18 @@ let a7 = a6.concat(['d'])
 console.log('a6: ', a6)
 console.log('a7: ', a7)
 
-// 1.4.2 splice(), 会修改原数组
+// 1.4.2 slice(), 不会修改原数组
+let a10 = [1, 2, 3]
+let a11 = a10.slice(1, 2)
+console.log('a10: ', a10)
+console.log('a11: ', a11)
+
+// 1.4.3 splice(), 会修改原数组
 let a8 = ['a', 'b', 'c']
 let a9 = a8.splice(1, 1)
-console.log('a8: ', a8)
-console.log('a9: ', a9)
+console.log('a8: ', a8) // ['a', 'c']
+console.log('a9: ', a9) // ['b']
+
 
 
 
